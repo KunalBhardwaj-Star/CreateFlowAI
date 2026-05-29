975ccb2fba2ded36bc3325512d2ec92621cc2e2e
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-29 18:42:04 +0530

    Database and Backend connected successfully!!

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,45 +11,18 @@ app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
 
-app.post("/test", async (req, res) => {
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "llama-3.3-70b-versatile",
-            messages: [
-                {
-                    role: "user",
-                    content: req.body.message
-                }
-            ]
-        })
-    };
-
+const connectDB = async () => {
     try {
-        const response = await fetch(
-            "https://api.groq.com/openai/v1/chat/completions",
-            options
-        );
-
-        const data = await response.json();
-
-        //console.log(data);
-
-        res.json({
-            message: data.choices[0].message.content
-        });
-
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
     } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            error: "Something went wrong"
-        });
+        console.error("Error connecting to MongoDB:", error);
     }
-});
+};
+
+// app.post("/test", async (req, res) => {
+    
+// });

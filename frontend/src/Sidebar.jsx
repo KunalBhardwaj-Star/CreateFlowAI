83444b8392600d0ffefd0b8c236efebc2c7805d5
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-30 15:20:45 +0530

    Sidebar structure

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -3,9 +3,22 @@ import './Sidebar.css';
 export default function Sidebar(){
     return (
         <>
-            <div>
-                Sidebar
-            </div>
+            <section className='sidebar'>
+                <button>
+                    <img src="src/assets/blacklogo.png" alt="gpt logo" />
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+
+                <ul className='history'>
+                    <li>history 1</li>
+                    <li>history 2</li>
+                    <li>history 3</li>
+                </ul>
+
+                <div className='sign'>
+                    <p>By Kunal &hearts;</p>
+                </div>
+            </section>
         </>
     )
 }
